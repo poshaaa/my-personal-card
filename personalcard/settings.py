1a0854452a687d470183863a77a6456b13ccc388
poshaaa <99191635+poshaaa@users.noreply.github.com>
2022-09-20 01:16:30 +0400

    Update settings.py

--- personalcard/settings.py
+++ personalcard/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'skills.apps.SkillsConfig',
-    'importexport',
 ]
 
 MIDDLEWARE = [
@@ -128,8 +127,6 @@ STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-MYSQLDUMP = "/usr/local/bin/mysqldump"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

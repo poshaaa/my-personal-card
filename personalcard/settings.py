9d039543b88a7606ec4daf611fa40b35c75a7e75
poshaaa <99191635+poshaaa@users.noreply.github.com>
2022-09-20 01:11:25 +0400

    Update settings.py

--- personalcard/settings.py
+++ personalcard/settings.py
@@ -129,7 +129,10 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+MYSQLDUMP = "/usr/local/bin/mysqldump"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+

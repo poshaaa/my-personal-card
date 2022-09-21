18d075405db64ee25f5c87f44424a7a9d3ea4b54
poshaaa <99191635+poshaaa@users.noreply.github.com>
2022-09-21 15:46:08 +0400

    Update settings.py

--- personalcard/settings.py
+++ personalcard/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'personalcard.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
-        'USER': 'root',
-        'PASSWORD': 'polina27',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
+    'default': {                                            #если иструкция не сработает
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }                                                           
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',             #если иструкция сработает
+#         'NAME': 'database',
+#         'USER': 'root',
+#         'PASSWORD': 'polina27',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
